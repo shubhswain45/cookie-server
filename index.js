5cb138fd0274304d905cffc7187bf4fb89addefd
shubhswain45 <shubhkantswain@.com>
2024-11-26 16:33:30 +0530

    done

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cookieParser());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+
+// Route to set a cookie
+app.get('/set-cookie', (req, res) => {
+    res.cookie('dummyCookie', 'cookieValue123', { httpOnly: true, path: '/' });
+    res.send({ message: 'Cookie has been set!' });
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Express server running on http://localhost:${PORT}`);
+});
